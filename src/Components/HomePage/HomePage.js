64d100da7bc41d36a416e58d77f2d4384dc4766d
ms161 <msehrawat161>
2023-07-06 00:08:49 +0530

    Verify User Email

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
+  const token=localStorage.getItem('token')
+  const [verificationError,setVerficicatoinError]=useState(null)
+const verificationHandler=e=>{
+  verificationApi()
+
+}
+
+async function  verificationApi(){
+try
+{  const resp=await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBgWxt6RxW_LbQKHbJk3IHn2MgI1yhAp6o',{
+    method:"POST",
+    body: JSON.stringify({
+    
+      requestType:"VERIFY_EMAIL",
+      idToken:token,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+
+  })
+
+  const data=await resp.json()
+
+  if(resp.ok){
+    console.log(data)
+  }}
+  catch(error) {
+    console.log(error)
+    
+
+  }
+
+
+}
+
+
   return (
     <div className='flex justify-around  items-center '>
 
     <div className='text-[2rem]'>Welcome TO expense tracker</div>
     <div>YOur Profile is incomplete <Link to='/userProfile'> <button className='border bg-green-500 p-1 rounded-lg'>Complete now</button> </Link></div>
+    <button onClick={verificationHandler}>click here to verify ur Email</button>
     </div>
   )
 }
