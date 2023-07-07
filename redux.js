34812aaae66ced0bc10c18efd5e510404c201b12
ms161 <115150076+ms161@users.noreply.github.com>
2023-07-07 13:13:54 +0530

    Add files via upload

--- redux.js
+++ redux.js
@@ -10,6 +10,14 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "decrement") {
     return { counter: state.counter - 1 };
   }
+  if (action.type === "incrementBy2") {
+    return {
+      counter: state.counter + 2,
+    };
+  }
+  if (action.type === "decrementBy2") {
+    return { counter: state.counter - 2 };
+  }
   return state;
 };
 
@@ -21,7 +29,10 @@ const counterSubscriber = () => {
 };
 
 store.subscribe(counterSubscriber);
-for (let i = 0; i < 5; i++) {
+
   store.dispatch({ type: "increment" });
-}
+  store.dispatch({ type: "incrementBy2" });
+
 store.dispatch({ type: "decrement" });
+store.dispatch({ type: "decrementBy2" });
+
