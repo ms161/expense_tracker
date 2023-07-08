f85efc9e7dbc01a51f0f6776c9c51dc0691a8d45
ms161 <115150076+ms161@users.noreply.github.com>
2023-07-08 07:11:00 +0530

    Auth Reducer task 15

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
-import { useContext, useState } from "react";
-import LoginPage from "./Components/LoginPage/LoginPage";
-import SignUpPage from "./Components/SignUpPage/SignUpPage";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import AuthenticationCtx from "./Components/Authentication/Authentication";
-import HomePage from "./Components/HomePage/HomePage";
-import { useEffect } from "react";
-import UserProfile from "./Components/UserProfile/UserProfile";
-import ForgetPasword from "./Components/LoginPage/ForgetPasword";
-import UserExpense from "./Components/UserExpense/UserExpense";
+import Counter from './components/Counter';
+import Header from './components/Header'
+import Auth from './components/Auth';
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
+import UserProfile from './components/UserProfile'
 
 function App() {
-  const ctx = useContext(AuthenticationCtx);
-  const navigate = useNavigate();
-
-  // console.log(ctx.isLoggedIn);
-  console.log('app running')
- 
-  useEffect(() => {
-    console.log('useeffect running')
-    if (ctx.isLoggedIn) {
-      navigate("/home");
-    } else {
-      navigate("/login");
-    }
-  }, [ctx.isLoggedIn]);
+const isAuth=useSelector(state=>state.auth.isAuthenticated)
 
   return (
-    <div>
-      <Routes>
-        <Route index element={<SignUpPage />}></Route>
-
- { !ctx.isLoggedIn&&  <Route path="/login" element={<LoginPage></LoginPage>}></Route>}
-
-  {ctx.isLoggedIn&&  <Route path="/home" element={<HomePage></HomePage>}></Route>}
-        <Route path="/userProfile" element={<UserProfile></UserProfile>}></Route>
-        <Route path='/forget' element={<ForgetPasword></ForgetPasword>}></Route>
-        <Route path='/userExpense' element={<UserExpense></UserExpense>}></Route>
-      </Routes>
-    </div>
+    <>
+    <Header></Header>
+   {!isAuth&& <Auth></Auth>}
+   {isAuth&& <UserProfile></UserProfile>}
+    <Counter />
+    </>
   );
 }
 
